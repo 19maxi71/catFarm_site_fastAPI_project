25fdec3681a6c8d07cd931f2c97ff2d6b6ff2d9f
19maxi71 <131301678+19maxi71@users.noreply.github.com>
2025-07-13 00:07:25 +0200

    Linked 'admin' to db. Added alembic for migrations.

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,14 +1,1 @@
-from sqladmin import ModelView
-from ..models import Cat
-
-
-class CatAdmin(ModelView, model=Cat):
-    column_list = [Cat.id, Cat.name, Cat.breed,
-                   Cat.created_at]  # What to show in the list
-    column_details_list = [Cat.id, Cat.name, Cat.breed, Cat.description,
-                           Cat.created_at, Cat.updated_at]  # What to show when viewing one cat
-    can_create = True  # Can add new cats
-    can_edit = True    # Can change cat info
-    can_delete = True  # Can remove cats
-    name = "Cat"       # What to call one cat
-    name_plural = "Cats"  # What to call many cats
+from .cat_admin import CatAdmin
--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from sqladmin import Admin, ModelView
 from .database import Base, engine
 from .models import Cat
+from .admin import CatAdmin
 
 app = FastAPI()
 
@@ -10,6 +11,7 @@ Base.metadata.create_all(bind=engine)
 
 # Initialize admin
 admin = Admin(app, engine)
+admin.add_view(CatAdmin)
 
 @app.get("/")
 async def root():
